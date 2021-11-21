4b82450e725c22a9d024065f80015c4e3ab7155d
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-21 18:00:00 +0900

    Add scrapper part2

--- Scrapper/main.py
+++ Scrapper/main.py
@@ -7,8 +7,12 @@ indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 pagination = indeed_soup.find("div", {"class": "pagination"})
 
-pages = pagination.find_all('a')
-spans = []
-for page in pages:
-    spans.append(page.find("span"))
-spans = spans[:-1]  
+links = pagination.find_all('a')
+pages = []
+for link in links[:-1]:
+    pages.append(int(link.string)) 
+
+max_page = pages[-1]
+
+for n in range(max_page):
+    print(f"start={n*50}")
