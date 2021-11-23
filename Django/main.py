e0b16b9f1d5d1739085bad583f6dc89cc5763c79
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-23 15:43:05 +0900

    Add django

--- Django/main.py
+++ Django/main.py
@@ -11,12 +11,21 @@ class Car():
   def __str__(self):
     return f"Car with {self.wheels} wheels"
 
-porche=Car(color="green", price="$40")
-print(porche.color, porche.price)
 
+class Convertible(Car):
 
-mini = Car()
-print(mini.color, mini.price)
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    self.time = kwargs.get("time", 10)
+
+  def take_off(self):
+    return "taking off"
+
+  def __str__(self):
+    return f"Car with no roof"
+
+porche=Convertible(color="green", price="$40")
+print(porche.color)
 
 # class 안에 있으면 method, class 밖에 있으면 function
 # method에 첫번째 인자는 method를 호출하는 instance 자신
